9703fccf50fdc035216d3e7859bb9b0258b0cebf
ruana101 <ruanadalia@gmail.com>
2025-05-28 15:02:38 +0300

    Am modificat cautare zbor.

--- src/main/java/dao/CautareZborDAO.java
+++ src/main/java/dao/CautareZborDAO.java
@@ -6,25 +6,39 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class CautareZborDAO {
-    public static void cautaZboruri(String plecare, String sosire) {
-        String query = "SELECT * FROM zboruri WHERE oras_plecare = ? AND oras_sosire = ?";
+    public static String cautaZboruri(String plecare, String sosire, String data, int nrPersoane) {
+        StringBuilder rezultate = new StringBuilder();
+        String query = "SELECT * FROM zboruri WHERE oras_plecare = ? AND oras_sosire = ? AND data_plecare = ?";
+
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement st = conn.prepareStatement(query)) {
 
             st.setString(1, plecare);
             st.setString(2, sosire);
+            st.setString(3, data);
 
             ResultSet rs = st.executeQuery();
 
-            System.out.println("Zboruri găsite:");
+            boolean gasit = false;
             while (rs.next()) {
-                System.out.println(rs.getString("cod_cursa") + " | " +
-                        rs.getString("model_avion") + " | " +
-                        rs.getString("tip"));
+                gasit = true;
+                rezultate.append("Cod: ").append(rs.getString("cod_cursa")).append(" | ")
+                        .append("Model: ").append(rs.getString("model_avion")).append(" | ")
+                        .append("Tip: ").append(rs.getString("tip")).append(" | ")
+                        .append("Locuri economy: ").append(rs.getInt("locuri_economy")).append(" | ")
+                        .append("Locuri business: ").append(rs.getInt("locuri_business")).append("\n");
             }
+
+            if (!gasit) {
+                rezultate.append("Nu există curse disponibile pentru această rută.\n");
+            }
+
             rs.close();
         } catch (Exception e) {
             e.printStackTrace();
+            rezultate.append("Eroare la căutarea zborurilor.\n");
         }
+
+        return rezultate.toString();
     }
 }
